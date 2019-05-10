6fbe24584109d90541d8b609f50cff2148fa6b63
Shekhar Tyagi <36845555+notshekhar@users.noreply.github.com>
2019-05-10 22:33:41 +0530

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -6,20 +6,18 @@ async function delay(s) {
     setTimeout(resolve, s)
   })
 }
-let browser
-(async ()=>{
-  browser = await puppeteer.launch()
-  await action()
-})()
 
 async function action(){
+  let browser = await puppeteer.launch()
   let page = await browser.newPage()
   await page.goto("https://www.youtube.com/watch?v=eZkAxnuOrFw&feature=youtu.be")
   await page.waitForSelector("#owner-name > a")
   await page.click("#movie_player > div.ytp-chrome-bottom > div.ytp-chrome-controls > div.ytp-left-controls > button")
   await delay(500)
   await console.log(i++)
-  await page.close()
+  browser.close()
   await action()
 }
 
+
+action()
